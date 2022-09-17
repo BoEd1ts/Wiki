b1fa77cf2c603f57ddd4852840507f559206ae93
Administrator <1390922296@qq.com>
2022-09-17 19:13:54 +0800

    9.16 将文档变成左右布局，方便文档内容的编辑

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,90 +3,98 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '80px'  }"
     >
-      <p>
-        <a-form layout="inline" :model="param">
+      <a-row>
+        <a-col :span="8">
+          <p>
+            <a-form layout="inline" :model="param">
 
-          <a-form-item>
-            <a-button type="primary" @click="handleQuery()">
-              查询
-            </a-button>
-          </a-form-item>
-          <a-form-item>
-            <a-button type="primary" @click="add()">
-              新增
-            </a-button>
-          </a-form-item>
-        </a-form>
+              <a-form-item>
+                <a-button type="primary" @click="handleQuery()">
+                  查询
+                </a-button>
+              </a-form-item>
+              <a-form-item>
+                <a-button type="primary" @click="add()">
+                  新增
+                </a-button>
+              </a-form-item>
+            </a-form>
 
-      </p>
-      <!--列,key id,数据doc,分页,等待框,分页执行方法-->
-      <a-table
-          :columns="columns"
-          :row-key="record=>record.id"
-          :data-source="level1"
-          :loading="loading"
-          :pagination="false"
-      >
-        <template #cover="{text:cover}">
-          <img class="img-wh" v-if="cover" :src="cover" alt="avatar"/> <!--渲染图片-->
-        </template>
-        <template v-slot:action="{ text, record }">
-          <a-space size="small">
-            <a-button type="primary" @click="edit(record)">
-              编辑
-            </a-button>
-            <a-popconfirm
-                title="删除后不可恢复！确定？"
-                ok-text="是"
-                cancel-text="否"
-                @confirm="handleDelete(record.id)"
-                @cancel="cancel"
-            >
-              <a-button type="primary" >
-                删除
-              </a-button>
-            </a-popconfirm>
-          </a-space>
-        </template>
-      </a-table>
+          </p>
+          <!--列,key id,数据doc,分页,等待框,分页执行方法-->
+          <a-table
+              :columns="columns"
+              :row-key="record=>record.id"
+              :data-source="level1"
+              :loading="loading"
+              :pagination="false"
+          >
+            <template #cover="{text:cover}">
+              <img class="img-wh" v-if="cover" :src="cover" alt="avatar"/> <!--渲染图片-->
+            </template>
+            <template v-slot:action="{ text, record }">
+              <a-space size="small">
+                <a-button type="primary" @click="edit(record)">
+                  编辑
+                </a-button>
+                <a-popconfirm
+                    title="删除后不可恢复！确定？"
+                    ok-text="是"
+                    cancel-text="否"
+                    @confirm="handleDelete(record.id)"
+                    @cancel="cancel"
+                >
+                  <a-button type="primary" >
+                    删除
+                  </a-button>
+                </a-popconfirm>
+              </a-space>
+            </template>
+          </a-table>
+        </a-col>
+        <a-col :span="16">
+          <!--弹出电子书-->
+          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+            <a-form-item label="名称">
+              <a-input v-model:value="doc.name"/>
+            </a-form-item>
+            <a-form-item label="父文档">
+              <a-tree-select
+                  v-model:value="doc.parent"
+                  style="width: 100%"
+                  :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+                  :tree-data="treeSelectData"
+                  placeholder="请选择父文档"
+                  tree-default-expand-all
+                  :replaceFields="{title:'name',key:'id',value:'id'}"
+              >
+              </a-tree-select>
+            </a-form-item>
+            <!--下拉菜单-->
+
+            <a-form-item label="顺序">
+              <a-input v-model:value="doc.sort"/>
+            </a-form-item>
+            <a-form-item label="内容">
+              <div id="content"></div>
+            </a-form-item>
+          </a-form>
+
+        </a-col>
+      </a-row>
+
     </a-layout-content>
 
   </a-layout>
-  <a-modal
-      title="分类管理"
-      v-model:visible="modalVisible"
-      :confirm-loading="modalLoading"
-      ok-text="ojbk"
-      @ok="handleModalOk"
-  >
-    <!--弹出电子书-->
-    <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-      <a-form-item label="名称">
-        <a-input v-model:value="doc.name"/>
-      </a-form-item>
-      <a-form-item label="父文档">
-        <a-tree-select
-            v-model:value="doc.parent"
-            style="width: 100%"
-            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-            :tree-data="treeSelectData"
-            placeholder="请选择父文档"
-            tree-default-expand-all
-            :replaceFields="{title:'name',key:'id',value:'id'}"
-        >
-        </a-tree-select>
-      </a-form-item>
-      <!--下拉菜单-->
+<!--  <a-modal-->
+<!--      title="分类管理"-->
+<!--      v-model:visible="modalVisible"-->
+<!--      :confirm-loading="modalLoading"-->
+<!--      ok-text="ojbk"-->
+<!--      @ok="handleModalOk"-->
+<!--  >-->
 
-      <a-form-item label="顺序">
-        <a-input v-model:value="doc.sort"/>
-      </a-form-item>
-      <a-form-item label="内容">
-        <div id="content"></div>
-      </a-form-item>
-    </a-form>
-
-  </a-modal>
+<!--  </a-modal>-->
 
 </template>
 
