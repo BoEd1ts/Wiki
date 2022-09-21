edd60057a27c9615a13899e27ceabf50d72f75a1
Administrator <1390922296@qq.com>
2022-09-21 08:24:36 +0800

    9.20 文档内容增加内容预览功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -83,10 +83,13 @@
               >
               </a-tree-select>
             </a-form-item>
-            <!--下拉菜单-->
-
             <a-form-item>
               <a-input v-model:value="doc.sort" placeholder="顺序"/>
+            </a-form-item>
+            <a-form-item>
+              <a-button type="primary" @click="handlePreviewContent()">
+                <EyeOutlined /> 内容预览
+              </a-button>
             </a-form-item>
             <a-form-item>
               <div id="content"></div>
@@ -96,8 +99,11 @@
         </a-col>
       </a-row>
 
-    </a-layout-content>
+      <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
+        <div class="wangeditor" :innerHTML="previewHtml"></div>
+      </a-drawer>
 
+    </a-layout-content>
   </a-layout>
 <!--  <a-modal-->
 <!--      title="分类管理"-->
@@ -340,6 +346,17 @@ export default defineComponent({
       })
     };
 
+    //-----------富文本预览---------------------
+    const drawerVisible = ref(false);
+    const previewHtml = ref();
+    const handlePreviewContent = () =>{
+      const html = editor.txt.html();
+      previewHtml.value = html;
+      drawerVisible.value = true;
+    };
+    const onDrawerClose = () => {
+      drawerVisible.value=false;
+    };
 
     onMounted(() => {
       handleQuery();
@@ -365,7 +382,12 @@ export default defineComponent({
       modalVisible,
       modalLoading,
       handleSave,
-      treeSelectData
+      treeSelectData,
+
+      drawerVisible,
+      previewHtml,
+      handlePreviewContent,
+      onDrawerClose,
     }
   }
 });
