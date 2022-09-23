fbc11e0198043f3e46802c1aaea76fdd14ca1efa
Administrator <1390922296@qq.com>
2022-09-23 17:45:04 +0800

    9.23 密码的两层加密处理

--- src/main/java/com/wu/wiki/req/UserSaveReq.java
+++ src/main/java/com/wu/wiki/req/UserSaveReq.java
@@ -1,7 +1,5 @@
 package com.wu.wiki.req;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -12,8 +10,8 @@ public class UserSaveReq {
     @NotNull(message = "[昵称] 不能为空")
     private String name;
     @NotNull(message = "[密码] 不能为空")
-    @Length(min = 6 , max = 20,message = "[密码]6~20位")
-    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$",message = "[密码]至少包含 数字和英文长度6-20")
+    //@Length(min = 6 , max = 32,message = "[密码]6~32位")
+    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$",message = "[密码]至少包含 数字和英文长度6-32")
     private String password;
 
     public Long getId() {
