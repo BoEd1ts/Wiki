b98c3b2f6f6dc0c7f9565b85ada823ff1dbb1793
Administrator <1390922296@qq.com>
2022-09-25 14:49:35 +0800

    9.25 前端调试成功，完成基本的登入功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -47,13 +47,18 @@
 </template>
 <script lang="ts">
 import { defineComponent,ref } from 'vue';
+import axios from "axios";
+import {message} from "ant-design-vue";
+
+declare let hexMd5:any;
+declare let KEY:any;
+
 
 export default defineComponent({
   name: 'the-header',
   setup () {
     const user = ref();
     user.value={};
-
 
 
     const loginUser = ref({
@@ -66,10 +71,24 @@ export default defineComponent({
       loginModalVisible.value = true;
     };
 
-    // 退出登录
+    // 登录
     const login = () => {
       console.log("开始登录");
+      loginModalLoading.value = true;
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+      axios.post('/user/login', loginUser.value).then((response) => {
+        loginModalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          loginModalVisible.value = false;
+          message.success("登录成功！");
+
+        } else {
+          message.error(data.message);
+        }
+      });
     };
+
 
 
     return {
