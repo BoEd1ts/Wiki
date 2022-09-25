c9ae4e366364715c48797d77787c81e689181845
Administrator <1390922296@qq.com>
2022-09-25 17:12:25 +0800

    9.25 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/wu/wiki/controller/UserControl.java
+++ src/main/java/com/wu/wiki/controller/UserControl.java
@@ -1,5 +1,6 @@
 package com.wu.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wu.wiki.req.UserLoginReq;
 import com.wu.wiki.req.UserQueryReq;
 import com.wu.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,30 @@ import com.wu.wiki.resp.PageResp;
 import com.wu.wiki.resp.UserLoginResp;
 import com.wu.wiki.resp.UserQueryResp;
 import com.wu.wiki.service.UserService;
+import com.wu.wiki.utils.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 //@Controller  返回页面 @RestController 是返回字符串的
 @RestController  //@ResponseBody用来返回字符串或JSON对象 大多是JSON对象
 @RequestMapping("/user")
 public class UserControl {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserControl.class);
     @Resource
     private UserService userService;
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     @RequestMapping("/list")  //接口支持所有的请求方式
     public CommonResp list(@Valid UserQueryReq req){
@@ -57,6 +70,12 @@ public class UserControl {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp <UserLoginResp>resp = new CommonResp();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登入token：{}，并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp),3600*24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
--- src/main/java/com/wu/wiki/resp/UserLoginResp.java
+++ src/main/java/com/wu/wiki/resp/UserLoginResp.java
@@ -1,13 +1,14 @@
 package com.wu.wiki.resp;
 
-import javax.validation.constraints.NotEmpty;
-
 public class UserLoginResp {
     private Long id;
-    @NotEmpty(message = "[用户名] 不能为空")
+
     private String loginName;
-    @NotEmpty(message = "[昵称] 不能为空")
+
     private String name;
+
+    private String token;
+
 
     public Long getId() {
         return id;
@@ -33,17 +34,21 @@ public class UserLoginResp {
         this.name = name;
     }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token='" + token + '\'' +
+                '}';
     }
 }
