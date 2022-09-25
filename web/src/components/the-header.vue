34e06adbe5da57bdc7bf63e02921b54ad125de3f
Administrator <1390922296@qq.com>
2022-09-25 18:59:30 +0800

    9.25 vuex整合sessionstorage，解决刷新数据丢失问题

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -49,7 +49,7 @@
   </a-layout-header>
 </template>
 <script lang="ts">
-import { defineComponent,ref } from 'vue';
+import {computed, defineComponent, ref} from 'vue';
 import axios from "axios";
 import {message} from "ant-design-vue";
 import store from "@/store";
@@ -62,8 +62,7 @@ export default defineComponent({
   name: 'the-header',
   setup () {
 
-    const user = ref();
-    user.value={};
+    const user = computed(() => store.state.user);
 
 
     const loginUser = ref({
@@ -87,7 +86,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
-          user.value= data.content;
+
           store.commit("setUser",user.value)
 
         } else {
